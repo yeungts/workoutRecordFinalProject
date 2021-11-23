174dd9582048c08563c3e324cff255d020938aee
kenye <kenye@KENDRICK>
2021-11-23 11:11:54 -0500

    Initial UI update

--- app/src/main/java/project/stn991524577_991473606/androidfinalproj/MainActivity.kt
+++ app/src/main/java/project/stn991524577_991473606/androidfinalproj/MainActivity.kt
@@ -2,10 +2,18 @@ package project.stn991524577_991473606.androidfinalproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar_main))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
