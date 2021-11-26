77f360dc6464610fa67b345eec6af99973ecf16d
kenye <kenye@KENDRICK>
2021-11-25 23:25:12 -0500

    updated main activity with data binding

--- app/src/main/java/project/stn991524577_991473606/androidfinalproj/MainActivity.kt
+++ app/src/main/java/project/stn991524577_991473606/androidfinalproj/MainActivity.kt
@@ -3,17 +3,39 @@ package project.stn991524577_991473606.androidfinalproj
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import project.stn991524577_991473606.androidfinalproj.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar_main))
+
+        binding.btnCheckRecords.setOnClickListener {
+            Toast.makeText(this, "check records clicked", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_favorite) {
+            Toast.makeText(this, "favourite clicked", Toast.LENGTH_LONG).show()
+            return true
+        }
         return true
     }
 }
