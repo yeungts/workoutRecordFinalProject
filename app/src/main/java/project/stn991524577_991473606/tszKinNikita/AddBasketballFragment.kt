643ea5d50c9e5571950726c1e7aec5fedf90eca2
Nikita <fedotov@sheridancollege.ca>
2021-12-09 15:04:45 -0500

    Added Configurations for FireBase

--- app/src/main/java/project/stn991524577_991473606/tszKinNikita/AddBasketballFragment.kt
+++ app/src/main/java/project/stn991524577_991473606/tszKinNikita/AddBasketballFragment.kt
@@ -1,10 +1,14 @@
 package project.stn991524577_991473606.tszKinNikita
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.FirebaseFirestore
 import project.stn991524577_991473606.tszKinNikita.databinding.FragmentAddBasketballBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,7 +25,7 @@ class AddBasketballFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    val fireStoreDatabase = FirebaseFirestore.getInstance()
     private var _binding: FragmentAddBasketballBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -47,6 +51,57 @@ class AddBasketballFragment : Fragment() {
 
         binding.addButton.setOnClickListener {
             var action = AddBasketballFragmentDirections.actionAddBasketballFragmentToWorkoutListFragment2()
+
+
+            var date : String = binding.date.text.toString()
+            var time : Int = binding.workoutLength.text.toString().toInt()
+            var distance : Double = binding.distance.text.toString().toDouble()
+            var points : Int = binding.points.text.toString().toInt()
+            var assists : Int = binding.assists.text.toString().toInt()
+            var rebounds : Int = binding.rebounds.text.toString().toInt()
+
+            val basketballWorkout : MutableMap<String, Any> = HashMap()
+
+            basketballWorkout["date"] = date
+            basketballWorkout["assists"] = assists
+            basketballWorkout["distance"] = distance
+            basketballWorkout["points"] = points
+            basketballWorkout["rebounds"] = rebounds
+            basketballWorkout["time"] = time
+            basketballWorkout["userId"] = "/users/cLvaECuNLAjgGoE9vZx1"
+
+            if (date.isNullOrEmpty() || assists.equals(null) || distance.equals(null) || points.equals(null) || rebounds.equals(null) || time.equals(null)){
+                val builder = AlertDialog.Builder(requireActivity())
+                builder.setTitle("")
+                builder.setMessage("Please fill in all fielfs to proceed with creating workout record.")
+                builder.setNeutralButton("OK") { dialog, which ->
+
+                }
+                builder.show()
+            } else {
+                fireStoreDatabase.collection("basketballWorkouts")
+                    .add(basketballWorkout)
+                    .addOnSuccessListener {
+                        Log.d(TAG, "Added document with ID ${it.id}")
+
+                        if (action != null) {
+                            this.findNavController().navigate(action)
+                        }
+
+                    }
+                    .addOnFailureListener {
+                        Log.d(TAG, "Error adding document ${it}")
+                    }
+
+                binding.date.text!!.clear()
+                binding.workoutLength.text!!.clear()
+                binding.distance.text!!.clear()
+                binding.points.text!!.clear()
+                binding.assists.text!!.clear()
+                binding.rebounds.text!!.clear()
+            }
+
+
         }
 
         return view
--- app/src/main/java/project/stn991524577_991473606/tszKinNikita/AddClimbingFragment.kt
+++ app/src/main/java/project/stn991524577_991473606/tszKinNikita/AddClimbingFragment.kt
@@ -1,10 +1,14 @@
 package project.stn991524577_991473606.tszKinNikita
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.FirebaseFirestore
 import project.stn991524577_991473606.tszKinNikita.databinding.FragmentAddClimbingBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,7 +25,7 @@ class AddClimbingFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    val fireStoreDatabase = FirebaseFirestore.getInstance()
     private var _binding: FragmentAddClimbingBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -43,6 +47,55 @@ class AddClimbingFragment : Fragment() {
         _binding = FragmentAddClimbingBinding.inflate(inflater, container, false)
 
         val view = binding.root
+
+        binding.addButton.setOnClickListener {
+            var action = AddClimbingFragmentDirections.actionAddClimbingFragmentToWorkoutListFragment()
+
+
+            var date : String = binding.date.text.toString()
+            var time : Int = binding.workoutLength.text.toString().toInt()
+            var steps : Int = binding.steps.text.toString().toInt()
+
+            val climbingWorkout : MutableMap<String, Any> = HashMap()
+
+            climbingWorkout["date"] = date
+            climbingWorkout["steps"] = steps
+            climbingWorkout["time"] = time
+            climbingWorkout["userId"] = "/users/cLvaECuNLAjgGoE9vZx1"
+
+            if (date.isNullOrEmpty() || steps.equals(null)  || time.equals(null)){
+                val builder = AlertDialog.Builder(requireActivity())
+                builder.setTitle("")
+                builder.setMessage("Please fill in all fielfs to proceed with creating workout record.")
+                builder.setNeutralButton("OK") { dialog, which ->
+
+                }
+                builder.show()
+            } else {
+                fireStoreDatabase.collection("climbingWorkouts")
+                    .add(climbingWorkout)
+                    .addOnSuccessListener {
+                        Log.d(TAG, "Added document with ID ${it.id}")
+
+                        if (action != null) {
+                            this.findNavController().navigate(action)
+                        }
+
+                    }
+                    .addOnFailureListener {
+                        Log.d(TAG, "Error adding document ${it}")
+                    }
+
+                binding.date.text!!.clear()
+                binding.workoutLength.text!!.clear()
+                binding.steps.text!!.clear()
+            }
+
+
+        }
+
+
+
         return view
     }
 
